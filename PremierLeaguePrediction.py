b2554586f2aa19f496c9dbb183f88b28d0f500eb
patelhrid <patelhrid@gmail.com>
2023-05-27 16:21:55 -0400

    Allows for a range of dates to be specified through the console for use in the model.

--- PremierLeaguePrediction.py
+++ PremierLeaguePrediction.py
@@ -1,33 +1,40 @@
 import pandas as pd
-from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import classification_report
-from sklearn.preprocessing import LabelEncoder
 
-# Step 1: Load the CSV data
+# Read the CSV file
 data = pd.read_csv('final_dataset.csv')
 
-# Step 2: Preprocess the data
-# Convert the outcome column to numerical labels
-le = LabelEncoder()
-data['FTR'] = le.fit_transform(data['FTR'])
+# Convert the 'Date' column to datetime format
+data['Date'] = pd.to_datetime(data['Date'], dayfirst=True)
 
-# Select relevant features for the model
-features = data[['HTGS', 'ATGS', 'HTGC', 'ATGC', 'HTP', 'ATP', 'HTFormPts', 'ATFormPts', 'HTWinStreak3', 'HTWinStreak5', 'HTLossStreak3', 'HTLossStreak5', 'ATWinStreak3', 'ATWinStreak5', 'ATLossStreak3', 'ATLossStreak5', 'HTGD', 'ATGD', 'DiffPts', 'DiffFormPts']]
+# Extract the year from the 'Date' column and store it in a new column 'Year'
+data['Year'] = data['Date'].dt.year
 
-# Select the target variable
-target = data['FTR']
+# Specify the range of years for inclusion in the decision tree
+start_year = int(input("Enter the start year: "))
+end_year = int(input("Enter the end year: "))
 
-# Step 3: Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.2, random_state=42)
+# Filter the data based on the specified year range
+filtered_data = data[(data['Year'] >= start_year) & (data['Year'] <= end_year)]
 
-# Step 4: Train the Decision Tree model
-decision_tree = DecisionTreeClassifier()
-decision_tree.fit(X_train, y_train)
+# Select the relevant features and target variable for the decision tree
+features = ['HTGS', 'ATGS', 'HTGC', 'ATGC', 'HTP', 'ATP', 'DiffPts', 'DiffFormPts']
+target = 'FTR'
 
-# Step 5: Evaluate the Decision Tree model
-y_pred = decision_tree.predict(X_test)
-print(classification_report(y_test, y_pred))
+# Split the data into training and testing sets
+X = filtered_data[features]
+y = filtered_data[target]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Step 6: Make predictions (optional)
-# ...
+# Train the decision tree model
+model = DecisionTreeClassifier()
+model.fit(X_train, y_train)
+
+# Make predictions on the test set
+y_pred = model.predict(X_test)
+
+# Generate the classification report
+report = classification_report(y_test, y_pred)
+print(report)
