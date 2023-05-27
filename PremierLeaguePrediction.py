2f0ec41abc7b03b90b6491103ae4d6c9267b476f
patelhrid <patelhrid@gmail.com>
2023-05-27 17:18:09 -0400

    Predictions working in a way that is more in line with expectation. Numbers involved with the predictions, however, don't make much sense.

--- PremierLeaguePrediction.py
+++ PremierLeaguePrediction.py
@@ -18,32 +18,47 @@ end_year = int(input("Enter the end year: "))
 # Filter the dataset based on the specified range of years
 filtered_data = data.loc[(data['Date'].str[-2:] >= str(start_year)[-2:]) & (data['Date'].str[-2:] <= str(end_year)[-2:])]
 
-# Split the data into features and target variable
+# Get the list of teams
+teams = sorted(set(filtered_data['HomeTeam']) | set(filtered_data['AwayTeam']))
+
+# Train a decision tree model for all matches
 X = filtered_data[['HTGS', 'ATGS', 'HTGC', 'ATGC', 'HTP', 'ATP']].values
 y = filtered_data['FTR'].values
 
-# Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-# Train the decision tree model
 decision_tree = DecisionTreeClassifier(random_state=42)
-decision_tree.fit(X_train, y_train)
+decision_tree.fit(X, y)
 
 # GUI for Match Prediction
 while True:
     print("Enter the Home Team:")
     home_team = input()
+    if home_team not in teams:
+        print("Invalid home team. Please select from the available teams.")
+        continue
 
     print("Enter the Away Team:")
     away_team = input()
+    if away_team not in teams:
+        print("Invalid away team. Please select from the available teams.")
+        continue
 
     # Prepare the input features for prediction
     home_stats = filtered_data.loc[filtered_data['HomeTeam'] == home_team].tail(1)[['HTGS', 'HTGC', 'HTP']].values
     away_stats = filtered_data.loc[filtered_data['AwayTeam'] == away_team].tail(1)[['ATGS', 'ATGC', 'ATP']].values
     input_features = np.concatenate((home_stats, away_stats), axis=1)
 
-    # Predict the match outcome
+    # Predict the match outcome using the decision tree model
     predicted_result = le.inverse_transform(decision_tree.predict(input_features))[0]
 
+    # Print the predicted result
     print("Predicted Result: " + predicted_result)
+    print("Key Numbers:")
+    print("Home Team: " + home_team)
+    print("Away Team: " + away_team)
+    print("Home Team Goals Scored: " + str(home_stats[0][0]))
+    print("Away Team Goals Scored: " + str(away_stats[0][0]))
+    print("Home Team Goals Conceded: " + str(home_stats[0][1]))
+    print("Away Team Goals Conceded: " + str(away_stats[0][1]))
+    print("Home Team Points: " + str(home_stats[0][2]))
+    print("Away Team Points: " + str(away_stats[0][2]))
     print("------------------------")
